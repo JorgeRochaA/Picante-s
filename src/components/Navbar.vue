250a777df0318298c4c09aa3e1c10198f46fd343
Jorge Rocha <jor2019.cr@gmail.com>
2021-08-10 17:27:17 -0600

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,101 @@
+<template>
+  <div>
+    <nav>
+      <div class="logo">
+        <img src="../assets/logo/logo.svg" alt="logo" />
+        <h3>Picante´s</h3>
+      </div>
+      <div class="links">
+        <h3>Contact Us</h3>
+        <a href=""><i class="fab fa-facebook"></i></a>
+        <a href=""><i class="fab fa-twitter"></i></a>
+        <a href=""><i class="fab fa-instagram"></i></a>
+      </div>
+      <div class="mobile-button">
+        <span><i class="fas fa-bars"></i></span>
+      </div>
+    </nav>
+  </div>
+</template>
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+<style scoped lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Monoton&display=swap");
+nav {
+  height: 100px;
+  width: 100%;
+  background-color: white;
+  box-shadow: 0px 3px 5px 0px rgba(0, 0, 0, 0.1);
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  font-size: 1.5rem;
+  font-family: "monoton";
+}
+nav img {
+  height: 80%;
+}
+.logo,
+.links {
+  height: 100%;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+}
+.logo,
+.links a {
+  color: black;
+}
+.logo {
+  width: 25%;
+}
+.links {
+  width: 75%;
+}
+.links a,
+.links h3 {
+  font-size: 1.7rem;
+  text-decoration: none;
+  transition: 0.5s;
+}
+.links a:hover,
+.links h3:hover {
+  transform: translateY(-10px);
+  cursor: pointer;
+}
+.mobile-button{
+  display: none;
+}
+@media (min-width: 320px) and (max-width: 480px) {
+  nav{
+   position: relative;
+   height: 500px;
+   flex-direction: column;
+  }
+  .logo img {
+    height: 50%;
+  }
+  .logo {
+    width: 70%;
+    font-size: 0.9rem;
+    justify-content: space-around;
+  }
+  .links {
+    position: absolute;
+    left: 0;
+    flex-direction: column;
+    flex-wrap: wrap;
+    background-color: firebrick;
+  }
+  .mobile-button{
+    height: 100%;
+    width: 30%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,59 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section id="main">
+      <div class="navbar-container">
+        <Navbar />
+      </div>
+      <div class="info-container">
+        <img src="../assets/skull-flags.png" alt="skull-flags" />
+        <h3>There’s no place like Picante´s</h3>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Navbar from "../components/Navbar.vue";
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    Navbar,
+  },
+};
 </script>
+
+<style scoped lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Monoton&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Bangers&display=swap");
+#main {
+  height: 100vh;
+  width: 100%;
+  background-image: url("../assets/banner/banner.jpg");
+  background-size: contain;
+  background-position-x: center;
+  display: flex;
+  flex-direction: column;
+}
+.navbar-container,
+.info-container {
+  width: 100%;
+}
+.navbar-container {
+  height: 10%;
+  background-color: chocolate;
+}
+.info-container {
+  height: 90%;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+}
+.info-container img {
+  height: 50%;
+}
+h3 {
+  font-size: 3.5rem;
+  color: black;
+  font-family: "Bangers";
+}
+</style>
