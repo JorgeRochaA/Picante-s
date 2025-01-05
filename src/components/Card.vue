c6371d0ce2e581bbc639dbfdc07f511dcc49bfb3
JorgeRochaa <jor2019.cr@gmail.com>
2025-01-04 23:40:50 -0600

    changes

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -7,9 +7,9 @@
       <div class="card-info">
         <h3 class="food-name">{{ foodItem.data.name }}</h3>
         <div class="price">
-          <h3 class="price-letters">Price</h3>
           <h3>$ {{ foodItem.data.price }}</h3>
         </div>
+        <h3 class="available">Available</h3>
       </div>
     </div>
   </div>
@@ -25,64 +25,63 @@ export default {
 </script>
 <style scoped lang="css">
 @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap");
-.card.firstColor {
-  background-color: #ea964e;
-}
-.card.secondColor {
-  background-color: #4f848a;
-}
 .card {
-  height: 220px;
-  width: 200px;
-  border-radius: 15px;
+  width: 192px;
+  height: 226px;
+  border-radius: 16px;
+  background: #1f1d2b;
+  border-radius: 16px;
   display: flex;
   flex-direction: column;
   align-items: center;
   position: relative;
   margin: 50px 0px;
   color: white;
-  box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px,
-    rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
 }
 .card-img {
-  height: 150px;
-  width: 150px;
+  height: 132px;
+  width: 132px;
   border-radius: 50%;
-  border: 10px solid white;
   position: absolute;
   top: -20%;
   overflow: hidden;
   display: flex;
   justify-content: center;
   align-items: center;
-  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
 }
 .card-img img {
   height: 100%;
 }
 .card-info {
-  height: 100px;
-  width: 100%;
+  position: absolute;
+  width: 144px;
+  height: 88px;
+  margin-top: 112px;
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
   align-items: center;
-  position: absolute;
-  bottom: 0;
+  font-family: "Roboto", sans-serif;
 }
 .food-name {
-  width: 90%;
   color: whitesmoke;
+  position: static;
+  color: rgb(255, 255, 255);
+  font-size: 14px;
+  font-weight: 500;
 }
 .price {
-  width: 70%;
-  font-family: "Roboto", sans-serif;
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
-  color: white;
+  margin-top: 15px;
+  font-size: 13.5px;
+  margin-bottom: 10px;
 }
-.price-letters {
-  color: black;
+
+h3.available {
+  color: rgb(171, 187, 194);
+  font-size: 13.5px;
+  font-weight: 400;
+  line-height: 140%;
+  letter-spacing: 0%;
+  text-align: center;
 }
 </style>
